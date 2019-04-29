3400084336b3ed1fa0d61609d432f95e8f0af4d1
Lexkane <40840623+Lexkane@users.noreply.github.com>
2019-04-29 20:26:53 +0300

    Add files via upload

---
+++ GoChans/GoSelect2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		for {
+			c1 <- "Every 500ms"
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "Every two seconds "
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	for {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
+		}
+	}
+}
